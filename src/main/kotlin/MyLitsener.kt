dee3330d24b4ea3e8efe7e6ac4730bcba5f6310e
heziblack <heziblackforfun@gmail.com>
2023-11-09 18:10:12 +0800

    daily save

--- src/main/kotlin/MyLitsener.kt
+++ src/main/kotlin/MyLitsener.kt
@@ -5,12 +5,24 @@ import net.mamoe.mirai.event.ListenerHost
 import net.mamoe.mirai.event.events.GroupMemberEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.message.data.content
+import net.mamoe.mirai.utils.MiraiLogger
+import java.nio.file.Path
 
 object MyListener:ListenerHost{
+    var dataFolder: Path? = null
+    var configFolder: Path? = null
+    var logger:MiraiLogger? = null
     @EventHandler
     suspend fun test(e:GroupMessageEvent){
-        if (e.message.content=="签到"){
-            e.group.sendMessage("签个屁")
+        if (e.message.content=="路径"){
+            e.group.sendMessage(dataFolder.toString()+"\n"+ configFolder.toString())
         }
     }
+    /**初始化操作, 保存插件的必要线索*/
+    public fun initFunc(dp:Path,cp:Path,lgr:MiraiLogger){
+        dataFolder = dp
+        configFolder = cp
+        logger = lgr
+    }
+
 }
--- src/main/kotlin/MyPluginMain.kt
+++ src/main/kotlin/MyPluginMain.kt
@@ -7,15 +7,21 @@ import net.mamoe.mirai.utils.info
 
 object MyPluginMain : KotlinPlugin(
         JvmPluginDescription(
-                id = "org.hezistudio.demo",
-                name = "Demo",
+                id = "org.hezistudio.groupmanager",
+                name = "hezi群管插件",
                 version = "0.1.0",
         ) {
-
             author("Hezi")
         }
 ) {
+
+
     override fun onEnable() {
+        MyListener.initFunc(dataFolderPath,configFolderPath,logger)
+        logger.info("初始化监听成功")
         globalEventChannel().registerListenerHost(MyListener)
+        logger.info("监听注册成功")
+
+        logger.info("插件加载完成")
     }
 }
